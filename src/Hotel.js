526bd3224fad8528b1d2542c635b983655445d6e
Carla Geglio <carla.geglio@gmail.com>
2020-01-12 10:39:51 -0700

    Add method to remove a reservation from hotel

--- src/Hotel.js
+++ src/Hotel.js
@@ -11,6 +11,10 @@ class Hotel {
     this.reservations.push(reservation);
   }
 
+  removeReservation(reservation) {
+    this.reservations.splice(this.reservations.indexOf(this.reservations.find(r => r.id === reservation.id)), 1);
+  }
+  
   findReservations(type, specific) {
     let reservations = this.reservations.filter(r => r[type] === specific);
     if (type === 'userID') {
