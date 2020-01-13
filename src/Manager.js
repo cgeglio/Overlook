81b41ec0f2b552d166e35529a8c24a9dba1a68f2
Carla Geglio <carla.geglio@gmail.com>
2020-01-13 15:41:22 -0700

    Add manager class that extends from user

---
+++ src/Manager.js
@@ -0,0 +1,15 @@
+import domUpdates from "../src/DomUpdate"
+import User from "../src/User"
+
+class Manager extends User {
+  constructor(reservations, hotel) {
+    super(reservations)
+    this.hotel = hotel;
+  }
+
+  updateHotelReservations() {
+    this.hotel.updateReservations(this.reservations);
+  }
+}
+
+export default Manager;
